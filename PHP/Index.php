e18745a9f0966263758b095fa727004db26ef09d
BrinkerhoffDevCode <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-10-30 20:46:32 -0300

    update - PHP

--- PHP/Index.php
+++ PHP/Index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <!-- Meta tags Obrigatórias -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,37 +10,53 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <title>Cadastro</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Cadastro</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="index.php">Home <span class="sr-only">(Página atual)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="novo-usuario.php">Novo Usuário</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="listar-usuario.php">Listar Usuário</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+</head>
 
-<?php
-    switch(@$_REQUEST["page"]){
-        case "novo":
-            include("novo-usuario.php")
-    }
-?>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Cadastro</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">Home <span class="sr-only">(Página atual)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=novo">Novo Usuário</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=listar">Listar Usuário</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+                <?php
+                include("config.php");
+                switch (@$_REQUEST["page"]) {
+                    case "novo":
+                        include("novo-usuario.php");
+                        break;
+                    case "listar";
+                        include("listar-usuario.php");
+                        break;
+                    default;
+                        print "<h1>Bem vindos!</h1>";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
 
     <script src="js/bootstrap.bundle.min.js"></script>
 
-    
-  </body>
+
+</body>
+
 </html>
---
+++ PHP/novo-usuario.php
@@ -0,0 +1,13 @@
+<h1>Novo Usuário</h1>
+<form>
+    <div class="mb-3">
+        <label>Nome</label>
+        <input type="text" name="nome" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>E-mail</label>
+        <input type="email" name="email" class=form-control">
+    </div>
+</form>
+
+<!-- Video: Como fazer CRUD com PHP e MySQL , Professor Eliel (Possui erros no codigo) --!>
